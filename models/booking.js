86035f2cb828f89395796d0b8de119e4d2a8f619
Betsy Astudillo <betsymalelyastudillo@hotmail.com>
2024-12-10 15:06:46 -0500

    se inicia modelado para reserva

---
+++ models/booking.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const BookingSchema = moongose.Schema({
+
+  room: {
+    type: String, 
+    required: true
+  },
+  roomId: {
+    type: String, 
+    required: true
+  },
+  userId: {
+    type: String, 
+    required: true
+  },
+  fromDate: {
+    type: String, 
+    required: true
+  },
+  toDate: {
+    type: String, 
+    required: true
+  },
+  totalDays: {
+    type: Number, 
+    required: true
+  },
+  transactionId: {
+    type: String, 
+    required: true
+  },
+  status: {
+    type: String, 
+    required: true,
+    default : 'booked'
+  }
+}, {
+  timestamps: true,
+})
+
+const bookingModel = mongoose.model('bookings', BookingSchema);
+
+module.exports = bookingModel;
