513606ce8db63c1921a074521a7c7c0cd14b7b58
MBustamante05 <securekali85@gmail.com>
2024-12-12 15:24:43 -0500

    Salas más reservadas

--- routes/roomsRoute.js
+++ routes/roomsRoute.js
@@ -6,7 +6,7 @@ const Room = require("../models/room");
 router.get("/getAllrooms", async (req, res) => {
   try {
     const rooms = await Room.find({});
-    return res.send(rooms)
+    return res.send(rooms);
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
@@ -14,24 +14,41 @@ router.get("/getAllrooms", async (req, res) => {
 router.post("/getRoomById", async (req, res) => {
   const { roomId } = req.body;
   try {
-    const room = await Room.findOne({_id: roomId});
-    return res.send(room)
+    const room = await Room.findOne({ _id: roomId });
+    return res.send(room);
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
 });
 
-router.post("/addroom", async(req,res) => {
+router.post("/addroom", async (req, res) => {
   try {
-    const newroom = new Room(req.body)
-    await newroom.save()
+    const newroom = new Room(req.body);
+    await newroom.save();
 
-    res.send('Agregar nueva sala');
-
+    res.send("Agregar nueva sala");
   } catch (error) {
-    return res.status(400).jason({error});
+    return res.status(400).jason({ error });
   }
-})
+});
 
+//Obtener las salas más reservadas
+router.get("/getTopRooms", async (req, res) => {
+  try {
+    const topRooms = await Room.aggregate([
+      {
+        $addFields: {
+          totalReservations: { $size: "$currentBookings" },
+        },
+      },
+      { $sort: { totalReservations: -1 } },
+      { $limit: 5 }, // Obtiene las 5 salas más reservadas
+    ]);
+
+    res.send(topRooms);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 module.exports = router;
